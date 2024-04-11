0059ed6fee2528af3fe09c384c74ef4e74bb786a
Valarmathy Danassegarane <valarmathy@mobilelabs.in>
2024-04-11 19:43:32 +0530

    refactor: cosmoops title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { GeistMono } from 'geist/font/mono'
 
 import '@/app/globals.css'
 import { cn } from '@/lib/utils'
-import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
 import { Toaster } from '@/components/ui/sonner'
@@ -11,14 +10,14 @@ import { Toaster } from '@/components/ui/sonner'
 export const metadata = {
   metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'CosmoOps AI Chatbot',
+    template: `%s - CosmoOps AI Chatbot`
   },
   description: 'An AI-powered chatbot template built with Next.js and Vercel.',
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png'
+    icon: '/images/COlogo.png',
+    shortcut: '/images/COlogo.png',
+    apple: '/images/COlogo.png'
   }
 }
 
@@ -54,7 +53,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <Header />
             <main className="flex flex-col flex-1">{children}</main>
           </div>
-          {/* <TailwindIndicator /> */}
         </Providers>
       </body>
     </html>
